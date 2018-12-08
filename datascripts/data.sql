8f0c165cafc31c2d5c0f542ed080a3648fa5b91d
Binit Datta <binitdatta@gmail.com>
2018-12-08 15:14:54 -0600

    changed

--- datascripts/data.sql
+++ datascripts/data.sql
@@ -1,3 +1,21 @@
+INSERT INTO `ROLLINGSTONE_USER`
+(
+`first_name`,
+`last_name`,
+`member_type`,
+`registration_date`,
+`sex`,
+`user_name`,
+`account_number`)
+VALUES
+(
+'Binit',
+'Datta',
+'G',
+'2015-01-01',
+'M',
+'bdatta',
+'10001');
 
 INSERT INTO `rollingstone_account`
 (
